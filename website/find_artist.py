5855668bc408e416754f2a60e4052a6688d7b053
Sungjin Kang <sungjinkang@Sungjins-MacBook-Pro.local>
2024-04-07 12:48:38 -0400

    Fix resize in transform

--- website/find_artist.py
+++ website/find_artist.py
@@ -34,8 +34,7 @@ model.eval()
 
 # Define the image transformation
 transform = transforms.Compose([
-    transforms.Resize((256, 256)),
-    transforms.CenterCrop(224),
+    transforms.Resize((224, 224)),
     transforms.ToTensor(),  # Convert images to PyTorch tensors
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # Normalize the image
 ])
