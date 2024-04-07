3ad0f1b770290b38a98c8fb871b934b5b3e37b22
Sungjin Kang <sungjinkang@Sungjins-MacBook-Pro.local>
2024-04-07 12:21:32 -0400

    Finished extracting the most matched-artist from images.

--- website/app.py
+++ website/app.py
@@ -88,5 +88,5 @@ def choose_option(filename):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-    # app.run(debug=True)
+    # app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
--- website/make_animation.py
+++ website/make_animation.py
@@ -6,21 +6,6 @@ path = './uploads/'
 
 @make_animation_bp.route('/make_animation/<filename>', methods=['GET', 'POST'])
 def make_animation(filename=None):
-    # This happens after the best_guesses and descriptions are extracted. When the user firstly visits the page,
-    # it uses googleVision to extract best_guesses
-    if request.method == 'POST':
-        if request.form.get('correct') == 'yes':
-            # start chatbot
-            pass
-        else:
-            # ask the user to write the correct title
-            pass
-    else:
-        # extract the best_guesses
-        best_guesses, descriptions = detect_web(path + filename)
-
-
-
     return render_template_string('''
     <!doctype html>
     <html>
